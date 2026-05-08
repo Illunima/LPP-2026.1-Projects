c7d09686aa7c1f3673126ab25453b97bfcba3952
Illuma <111158234+Illunima@users.noreply.github.com>
2026-05-08 19:26:08 -0300

    Joining everything Again!

---
+++ CardBattlerGame/Main.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("Welcome to Python programming!")
+# This is a simple Python program that prints two lines of text to the console.
+# You can run this code in any Python environment to see the output.
